764d23bed0f106a346ad651cfe71b702d2f817ae
Italiyaharikrushn <italiyaharikrushn4@gmail.com>
2026-09-09 18:19:19 +0530

    Remove unused ARRAY import from user model

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ARRAY, String, Boolean, DateTime, Date, Table
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from db.base_class import Base
 from sqlalchemy.orm import relationship
 
