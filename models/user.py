7d8b7434d7985308d1017b43bff7ab672f67c1c4
Hk Italiya <153975683+Italiyaharikrushn@users.noreply.github.com>
2026-09-09 16:54:41 +0530

    Increase password field length to 255 characters

--- models/user.py
+++ models/user.py
@@ -13,7 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     first_name = Column((String(32)), nullable=False)
     last_name = Column((String(32)), nullable=False)
-    password = Column((String()), nullable=False)
+    password = Column((String(255)), nullable=False)
     email = Column((String(256)), unique=True, nullable=False)
     phone = Column((String(15)), nullable=True)
     gender = Column((String(15)), nullable=True)
